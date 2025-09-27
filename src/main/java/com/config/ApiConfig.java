a5aecc5e2598ab4f501c1057148ccc9aee65469f
mstamatovic <milosstamatovic984@gmail.com>
2025-09-27 16:19:14 +0200

    added ApiConfig constants class

---
+++ src/main/java/com/config/ApiConfig.java
@@ -0,0 +1,12 @@
+package com.config;
+
+public class ApiConfig {
+
+    public static final String BASE_URL = "https://restful-booker.herokuapp.com";
+    public static final String AUTH_TOKEN = BASE_URL + "/auth";
+    public static final String BOOKING = "/booking";
+    public static final String CONTENT_TYPE = "application/json";
+
+    public static final String username = "admin";
+    public static final String password = "password123";
+}
